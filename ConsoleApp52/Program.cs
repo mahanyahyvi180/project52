45c0936f27bbae88f7261cdc5afcd0e3a393eae2
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-06 22:54:37 +0330

    Add files via upload

---
+++ ConsoleApp52/Program.cs
@@ -0,0 +1,31 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giraff\nAcademy");
+
+            Phrase.ToUpper();
+        }
+
+        private static void Main1()
+        {
+            string phrase = "Giraffe Academy" + "is cool";
+
+            Console.WriteLine(phrase.Length);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
